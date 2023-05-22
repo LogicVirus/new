89f045f8bb88f5677e88e18e7cd9c09937635e91
Josh <98907510+LogicVirus@users.noreply.github.com>
2023-05-22 02:00:54 -0400

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -20,7 +20,7 @@ export default function PostPage({
   return (
     <Container>
       <Head>
-        <title>{post.title} | My awesome blog</title>
+        <title>{post.title} | Live Long and Prosper</title>
       </Head>
 
       {router.isFallback ? (
