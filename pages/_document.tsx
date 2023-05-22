bcafa939ceee70dd97b3e21a20635f56b7341feb
Josh <98907510+LogicVirus@users.noreply.github.com>
2023-05-22 01:58:37 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  const script = `
-    (function() {
-      const theme = localStorage.getItem('theme') || 'light';
-      document.querySelector('body').className = theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-gray-700';
-    })()
-  `;
-
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
         <meta name="robots" content="follow, index" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <script dangerouslySetInnerHTML={{ __html: script }}/>
       </Head>
-      <body>
+      <body className="bg-gray-200 text-gray-700 antialiased">
         <Main />
         <NextScript />
       </body>
