575b4469e783b28750bd8b441e22272b21862019
Josh <98907510+LogicVirus@users.noreply.github.com>
2023-05-22 02:56:42 -0400

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -39,7 +39,7 @@ export default function PostPage({
             </header>
 
             <div
-              className="prose mt-10"
+              className="dark-prose mt-10"
               dangerouslySetInnerHTML={{ __html: post.content }}
             />
           </article>
