70ebef97b7b87e3a206c210c041e4fbfe0ab15c9
Josh <98907510+LogicVirus@users.noreply.github.com>
2023-05-22 03:51:26 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,13 @@ function HomePage() {
             Welcome to StarCoder's Log!
           </h1>
           <p className="text-xl">
-            Greetings, fellow explorers! I'm Josh Perry, once an IT Ensign now turned Software Commander. Here in this digital cosmos, I venture into the nebula of Next.js, unravel the mysteries of next-mdx, and transform binary frontiers into user-friendly galaxies.
+            Greetings, explorers! I'm Josh Perry. Here in this digital cosmos, I venture into the nebula of Next.js and transform binary frontiers into user-friendly galaxies.
           </p>
           <p className="text-xl">
-            This interstellar hub, my first major foray into the coding universe, is a testament to my journey. Join me, as we engage warp drive and explore this ever-evolving tech universe together. 
+            This interstellar hub, my first major foray into the coding universe, stands as a testament to my journey. Join me, as we engage warp drive and explore this ever-evolving tech universe together. 
           </p>
           <p className="text-xl">
-            Strap in and prepare for launch...our adventure is just getting started!
+            Initiate star chart plotting...our odyssey is about to begin!
           </p>
         </div>
       </Container>
