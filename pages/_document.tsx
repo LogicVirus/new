a3e388c2ece725d3782048f7ff1b71c3a7313ddb
Josh <98907510+LogicVirus@users.noreply.github.com>
2023-05-22 02:21:12 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <meta name="robots" content="follow, index" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       </Head>
-      <body className="bg-gray-900 text-gray-100 antialiased">
+      <body className="bg-gray-900 text-gray-300 antialiased">
         <Main />
         <NextScript />
       </body>
