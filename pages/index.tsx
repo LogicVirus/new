9e5549dadfc15411ba288c7013434c45d64f9b12
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-23 02:36:16 -0400

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,44 +7,84 @@ import Image from 'next/image'
 function HomePage() {
   const twitterLink = "https://twitter.com/joshperry0";
   const flickrLink = "https://www.flickr.com/people/191854139@N02/";
-  const chatLink = "/chat";
 
   const controls = useAnimation();
 
+  useEffect(() => {
+    controls.start({
+      opacity: [0.5, 1, 0.5, 1],
+      transition: { duration: 6, ease: "easeInOut", times: [0, 0.33, 0.66, 1] },
+    }).then(() => {
+      controls.start({
+        opacity: 1,
+        transition: { duration: 2 }
+      });
+    });
+  }, [controls]);
+
   return (
-    <Container>
-      <div className="flex flex-col items-center mt-12">
-        <motion.button 
-          href={twitterLink}
-          className="twitter-gradient py-2 px-4 rounded"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          animate={controls}
-        >
-          Follow me on Twitter
-        </motion.button>
-        <motion.button 
-          href={flickrLink}
-          className="flickr-gradient py-2 px-4 rounded mt-4"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          animate={controls}
-        >
-          Visit my Flickr
-        </motion.button>
-        <Link href={chatLink}>
+    <motion.div 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <Container>
+        <div className="space-y-6 text-white">
+          <motion.h1 
+            className="text-3xl font-bold text-yellow-300"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', stiffness: 120 }}
+          >
+            Welcome to my Log!
+          </motion.h1>
+          <motion.p 
+            className="text-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }}
+          >
+            Greetings, explorers! I'm Josh Perry.
+          </motion.p>
           <motion.button 
-            className="chat-gradient py-2 px-4 rounded mt-4"
+            onClick={() => window.open(twitterLink, "_blank")} 
+            className="twitter-gradient py-2 px-4 rounded"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Follow me on Twitter
+          </motion.button>
+          <motion.button 
+            onClick={() => window.open(flickrLink, "_blank")} 
+            className="flickr-gradient py-2 px-4 rounded mt-4"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             animate={controls}
           >
-            Join the Chat
+            Visit my Flickr
           </motion.button>
-        </Link>
+          <Link href="/chat"> {/* Add this line */}
+            <motion.button 
+              className="chat-gradient py-2 px-4 rounded mt-4"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Join the Chat
+            </motion.button>
+          </Link> {/* And this line */}
+        </div>
+      </Container>
+
+      <div className="container max-w-4xl m-auto mt-20 bg-gray-900">
+        <Image
+          src="/1.png"
+          alt="A nebula signifying the uncharted territories of software development"
+          width={1920 / 2}
+          height={1280 / 2}
+        />
       </div>
-    </Container>
-  );
+    </motion.div>
+  )
 }
 
-export default HomePage;
+export default HomePage
