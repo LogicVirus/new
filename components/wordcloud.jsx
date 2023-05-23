f2df7531aecae158305a421569eafd7fa371331e
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-23 13:19:36 -0400

    modified:   components/wordcloud.jsx

--- components/wordcloud.jsx
+++ components/wordcloud.jsx
@@ -1,75 +1,89 @@
-import React, { useEffect, useRef } from 'react';
-import axios from 'axios';
-import * as d3 from 'd3';
-import cloud from 'd3-cloud';
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
+import Container from '../components/container';
+import Image from 'next/image';
+import WordCloud from '../components/WordCloud'; // import the WordCloud component
 
-function WordCloud() {
-  const svgRef = useRef(null);
+function HomePage() {
+  const twitterLink = "https://twitter.com/joshperry0";
+  const flickrLink = "https://www.flickr.com/people/191854139@N02/";
+
+  const controlsFlickr = useAnimation();
+  const controlsTwitter = useAnimation();
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=4de4a8c952184ea7b39a2486f8e04390');
-        const newsData = response.data.articles.map((article) => article.title);
-        createWordCloud(newsData);
-      } catch (error) {
-        console.error('Failed to fetch news:', error);
-      }
+    const sequence = async (controls) => {
+      await controls.start({
+        opacity: [0.5, 1, 0.5, 1],
+        transition: { duration: 6, ease: "easeInOut", times: [0, 0.33, 0.66, 1] },
+      });
+
+      controls.start({
+        opacity: 1,
+        transition: { duration: 2 }
+      });
     };
 
-    const createWordCloud = (newsData) => {
-      // Concatenate all news titles
-      const text = newsData.join(' ');
-
-      // Compute word frequency
-      const words = text.split(' ');
-      const wordCount = words.reduce((acc, word) => {
-        acc[word] = (acc[word] || 0) + 1;
-        return acc;
-      }, {});
-
-      // Create data suitable for d3-cloud
-      const wordEntries = Object.entries(wordCount).map(([word, count]) => ({
-        text: word,
-        size: count,
-      }));
-
-      const layout = cloud()
-        .size([800, 600])
-        .words(wordEntries)
-        .padding(5)
-        .rotate(() => ~~(Math.random() * 2) * 90)
-        .font('Impact')
-        .fontSize((d) => Math.sqrt(d.size) * 10)
-        .on('end', draw);
-
-      layout.start();
-    };
-
-    const draw = (words) => {
-      const svg = d3.select(svgRef.current);
-      svg
-        .attr('width', layout.size()[0])
-        .attr('height', layout.size()[1])
-        .append('g')
-        .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`)
-        .selectAll('text')
-        .data(words)
-        .enter().append('text')
-        .style('font-size', (d) => `${d.size}px`)
-        .style('font-family', 'Impact')
-        .style('fill', (d, i) => d3.schemeCategory10[i % 10])
-        .attr('text-anchor', 'middle')
-        .attr('transform', (d) => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
-        .text((d) => d.text);
-    };
-
-    fetchNews();
-  }, []);
+    sequence(controlsFlickr);
+    sequence(controlsTwitter);
+  }, [controlsFlickr, controlsTwitter]);
 
   return (
-    <svg ref={svgRef}></svg>
-  );
+    <motion.div 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <Container>
+        <div className="space-y-6 text-white">
+          <motion.h1 
+            className="text-3xl font-bold text-blue-700"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', stiffness: 120 }}
+          >
+            Welcome to my Log!
+          </motion.h1>
+          <motion.p 
+            className="text-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }}
+          >
+            Greetings, explorers! I'm Josh Perry.
+          </motion.p>
+          <motion.button 
+            onClick={() => window.open(twitterLink, "_blank")} 
+            className="twitter-gradient py-2 px-4 rounded"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            animate={controlsTwitter}
+          >
+            Follow me on Twitter
+          </motion.button>
+          <motion.button 
+            onClick={() => window.open(flickrLink, "_blank")} 
+            className="flickr-gradient py-2 px-4 rounded mt-4"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            animate={controlsFlickr}
+          >
+            Visit my Flickr
+          </motion.button>
+        </div>
+        <WordCloud /> {/* Insert the WordCloud component here */}
+      </Container>
+
+      <div className="container max-w-4xl m-auto mt-20 bg-gray-900">
+        <Image
+          src="/1.png"
+          alt="A nebula signifying the uncharted territories of software development"
+          width={1920 / 2}
+          height={1280 / 2}
+        />
+      </div>
+    </motion.div>
+  )
 }
 
-export default WordCloud;
+export default HomePage
