7941e4bd6aa41dfd8c2c56a868e8434ef99f380c
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-22 23:35:13 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,3 +28,18 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </Auth0Provider>
   )
 }
+
+
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+ 
+export default MyApp;
