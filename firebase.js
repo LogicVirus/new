c442f4c2acc8032298b979d7353c1d12e295c4ab
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-23 00:24:09 -0400

    new file:   firebase.js 	modified:   package-lock.json 	modified:   package.json

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDv7X885Rly4ebEPfq4BZ-gf1yNBWnSUbo",
+  authDomain: "chat-9e8ae.firebaseapp.com",
+  projectId: "chat-9e8ae",
+  storageBucket: "chat-9e8ae.appspot.com",
+  messagingSenderId: "775860662448",
+  appId: "1:775860662448:web:b3d9dfe3193cc9886d4b75",
+  measurementId: "G-44NHR69BM4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
