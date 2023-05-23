c14c900489a2603b1df2fa3556168268ac374551
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-23 01:26:06 -0400

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,8 +47,8 @@ function HomePage() {
             Greetings, explorers! I'm Josh Perry.
           </motion.p>
 
-          <Link href="/login"><a>Chat</a></Link> 
-          
+          <Link href="/login"><a>Chat</a></Link>  
+
           <motion.button 
             onClick={() => window.open(twitterLink, "_blank")} 
             className="twitter-gradient py-2 px-4 rounded"
