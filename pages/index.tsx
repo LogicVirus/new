c0956ac9113454d5ce059ca9c571fdc06980c5be
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-23 23:52:06 -0400

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,16 +33,17 @@ function HomePage() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
+      className="flex items-center justify-center h-screen bg-gray-900"
     >
       <Container>
-        <div className="space-y-6 text-white">
+        <div className="space-y-6 text-white text-center">
           <motion.h1 
-            className="text-3xl font-bold text-blue-600"
+            className="text-6xl font-bold text-blue-600"
             initial={{ y: -100 }}
             animate={{ y: 0 }}
             transition={{ type: 'spring', stiffness: 120 }}
           >
-            Welcome to my Log!
+            Josh Perry
           </motion.h1>
           <motion.p 
             className="text-xl"
@@ -50,7 +51,7 @@ function HomePage() {
             animate={{ opacity: 1 }}
             transition={{ delay: 1, duration: 1 }}
           >
-            Greetings, explorers! I'm Josh Perry.
+            Software Developer | Photographer | Explorer
           </motion.p>
           <motion.button 
             onClick={() => window.open(twitterLink, "_blank")} 
@@ -64,23 +65,12 @@ function HomePage() {
           <motion.button 
             onClick={() => window.open(flickrLink, "_blank")} 
             className="flickr-gradient py-2 px-4 rounded mt-4"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
             animate={controlsFlickr}
           >
             Visit my Flickr
           </motion.button>
         </div>
       </Container>
-
-      <div className="container max-w-4xl m-auto mt-20 bg-gray-900">
-        <Image
-          src="/1.png"
-          alt="A nebula signifying the uncharted territories of software development"
-          width={1920 / 2}
-          height={1280 / 2}
-        />
-      </div>
     </motion.div>
   )
 }
