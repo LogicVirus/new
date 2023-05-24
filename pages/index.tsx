baf6f8219be097a935bae07cec6352e25789d2ec
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-24 00:03:27 -0400

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import Container from '../components/container'
 import Image from 'next/image'
 import style from '../styles/Home.module.css'
-
 function HomePage() {
   const twitterLink = "https://twitter.com/joshperry0";
   const flickrLink = "https://www.flickr.com/people/191854139@N02/";
@@ -33,45 +32,53 @@ function HomePage() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className="flex items-center justify-center h-screen bg-gray-900"
     >
-      <div className="mt-[-50px]">
-        <Container>
-          <div className="space-y-6 text-white text-center">
-            <motion.h1 
-              className="text-6xl font-bold text-blue-600"
-              initial={{ y: -100 }}
-              animate={{ y: 0 }}
-              transition={{ type: 'spring', stiffness: 120 }}
-            >
-              Josh Perry
-            </motion.h1>
-            <motion.p 
-              className="text-xl"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1, duration: 1 }}
-            >
-              Software Developer | Photographer | Explorer
-            </motion.p>
-            <motion.button 
-              onClick={() => window.open(twitterLink, "_blank")} 
-              className="twitter-gradient py-2 px-4 rounded"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              animate={controlsTwitter}
-            >
-              Follow me on Twitter
-            </motion.button>
-            <motion.button 
-              onClick={() => window.open(flickrLink, "_blank")} 
-              className="flickr-gradient py-2 px-4 rounded mt-4"
-              animate={controlsFlickr}
-            >
-              Visit my Flickr
-            </motion.button>
-          </div>
-        </Container>
+      <Container>
+        <div className="space-y-6 text-white">
+          <motion.h1 
+            className="text-3xl font-bold text-blue-600"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', stiffness: 120 }}
+          >
+            Welcome to my Log!
+          </motion.h1>
+          <motion.p 
+            className="text-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }}
+          >
+            Greetings, explorers! I'm Josh Perry.
+          </motion.p>
+          <motion.button 
+            onClick={() => window.open(twitterLink, "_blank")} 
+            className="twitter-gradient py-2 px-4 rounded"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            animate={controlsTwitter}
+          >
+            Follow me on Twitter
+          </motion.button>
+          <motion.button 
+            onClick={() => window.open(flickrLink, "_blank")} 
+            className="flickr-gradient py-2 px-4 rounded mt-4"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            animate={controlsFlickr}
+          >
+            Visit my Flickr
+          </motion.button>
+        </div>
+      </Container>
+
+      <div className="container max-w-4xl m-auto mt-20 bg-gray-900">
+        <Image
+          src="/1.png"
+          alt="A nebula signifying the uncharted territories of software development"
+          width={1920 / 2}
+          height={1280 / 2}
+        />
       </div>
     </motion.div>
   )
