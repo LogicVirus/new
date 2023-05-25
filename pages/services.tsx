d4ba4137b819be4d735b5e7145b87f34140e4174
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-25 13:31:45 -0400

    modified:   pages/services.tsx

--- pages/services.tsx
+++ pages/services.tsx
@@ -11,7 +11,7 @@ function ServicesPage() {
     >
       <Container>
         <motion.h1 
-          className="text-6xl font-bold text-yellow-600 text-center"
+          className="text-6xl font-bold text-yellow-600"
           initial={{ y: -100 }}
           animate={{ y: 0 }}
           transition={{ type: 'spring', stiffness: 120 }}
