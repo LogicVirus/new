55ef3d34d5746ca8ba48a7632435bb2eb50dcc46
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-25 13:09:16 -0400

    modified:   pages/services.tsx

--- pages/services.tsx
+++ pages/services.tsx
@@ -18,27 +18,34 @@ function ServicesPage() {
         >
           Services
         </motion.h1>
+       // ...rest of your code...
 
-
-<div className="flex flex-col space-y-4 mt-8">
+<div className="flex flex-col space-y-10 mt-8">
   <motion.div 
-    className="service-card bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white p-6 rounded-lg shadow-lg"
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
+    className="service-card bg-gray-100 p-6 rounded-lg shadow-lg flex items-start space-x-4"
+    whileHover={{ scale: 1.05 }}
+    whileTap={{ scale: 0.95 }}
   >
-    <h2 className="text-3xl">Software Development</h2>
-    <p>I specialize in creating efficient, scalable, and robust software solutions using modern web technologies like React, Node.js, and Next.js. Whether you need a full-stack application or a simple frontend, I can help.</p>
+    <img src="/software-development-icon.png" alt="Software Development Icon" className="w-12 h-12" />
+    <div>
+      <h2 className="text-2xl font-semibold text-blue-800">Software Development</h2>
+      <p className="mt-2">I specialize in creating efficient, scalable, and robust software solutions using modern web technologies like React, Node.js, and Next.js. Whether you need a full-stack application or a simple frontend, I can help.</p>
+    </div>
   </motion.div>
   <motion.div 
-    className="service-card bg-gradient-to-r from-green-400 via-blue-500 to-purple-500 text-white p-6 rounded-lg shadow-lg"
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
+    className="service-card bg-gray-100 p-6 rounded-lg shadow-lg flex items-start space-x-4"
+    whileHover={{ scale: 1.05 }}
+    whileTap={{ scale: 0.95 }}
   >
-    <h2 className="text-3xl">Technical Writing</h2>
-    <p>I can provide clear, concise technical documentation, tutorials, and blog posts to help your users or team understand your software product. I have experience writing for both technical and non-technical audiences.</p>
+    <img src="/technical-writing-icon.png" alt="Technical Writing Icon" className="w-12 h-12" />
+    <div>
+      <h2 className="text-2xl font-semibold text-blue-800">Technical Writing</h2>
+      <p className="mt-2">I can provide clear, concise technical documentation, tutorials, and blog posts to help your users or team understand your software product. I have experience writing for both technical and non-technical audiences.</p>
+    </div>
   </motion.div>
 </div>
 
+// ...rest of your code...
 
       </Container>
     </motion.div>
