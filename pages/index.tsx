164d1aa2b98b52125d1d4a9956d42e7e24522623
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-25 01:39:39 -0400

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { motion, useAnimation } from 'framer-motion';
 import { useEffect } from 'react';
 import Container from '../components/container'
 import Image from 'next/image'
+import { Analytics } from '@vercel/analytics/react';
 
 function HomePage() {
   const twitterLink = "https://twitter.com/joshperry0";
@@ -28,60 +29,63 @@ function HomePage() {
   }, [controlsFlickr, controlsTwitter]);
 
   return (
-    <motion.div 
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      className="flex flex-col items-center justify-center min-h-screen text-white"
-    >
-      <Container>
-        <motion.h1 
-          className="text-6xl font-bold text-blue-800 text-center"
-          initial={{ y: -100 }}
-          animate={{ y: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
-        >
-          Josh Perry
-        </motion.h1>
-        <motion.p 
-          className="text-2xl text-center text-yellow-600 mt-6"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1, duration: 1 }}
-        >
-          Greetings, explorers! Welcome to my portfolio.
-        </motion.p>
-        <div className="flex flex-col space-y-4 mt-8">
-          <motion.button 
-            onClick={() => window.open(twitterLink, "_blank")} 
-            className="twitter-gradient py-2 px-4 rounded text-lg"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            animate={controlsTwitter}
+    <>
+      <motion.div 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="flex flex-col items-center justify-center min-h-screen text-white"
+      >
+        <Container>
+          <motion.h1 
+            className="text-6xl font-bold text-blue-800 text-center"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', stiffness: 120 }}
           >
-            Follow me on Twitter
-          </motion.button>
-          <motion.button 
-            onClick={() => window.open(flickrLink, "_blank")} 
-            className="flickr-gradient py-2 px-4 rounded text-lg"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            animate={controlsFlickr}
+            Josh Perry
+          </motion.h1>
+          <motion.p 
+            className="text-2xl text-center text-yellow-600 mt-6"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }}
           >
-            Visit my Flickr
-          </motion.button>
-        </div>
-      </Container>
+            Greetings, explorers! Welcome to my portfolio.
+          </motion.p>
+          <div className="flex flex-col space-y-4 mt-8">
+            <motion.button 
+              onClick={() => window.open(twitterLink, "_blank")} 
+              className="twitter-gradient py-2 px-4 rounded text-lg"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              animate={controlsTwitter}
+            >
+              Follow me on Twitter
+            </motion.button>
+            <motion.button 
+              onClick={() => window.open(flickrLink, "_blank")} 
+              className="flickr-gradient py-2 px-4 rounded text-lg"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              animate={controlsFlickr}
+            >
+              Visit my Flickr
+            </motion.button>
+          </div>
+        </Container>
 
-      <div className="container max-w-4xl m-auto mt-20">
-        <Image
-          src="/qwert.png"
-          alt="A nebula signifying the uncharted territories of software development"
-          width={1920 / 2}
-          height={1280 / 2}
-        />
-      </div>
-    </motion.div>
+        <div className="container max-w-4xl m-auto mt-20">
+          <Image
+            src="/qwert.png"
+            alt="A nebula signifying the uncharted territories of software development"
+            width={1920 / 2}
+            height={1280 / 2}
+          />
+        </div>
+      </motion.div>
+      <Analytics />
+    </>
   )
 }
 
