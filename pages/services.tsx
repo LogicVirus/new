1b479fe6968e8490563ed046f55a3e99a5b1977c
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-25 13:13:46 -0400

    modified:   pages/services.tsx

--- pages/services.tsx
+++ pages/services.tsx
@@ -18,24 +18,26 @@ function ServicesPage() {
         >
           Services
         </motion.h1>
-        <div className="flex flex-col space-y-4 mt-8">
-          <motion.div 
-            className="service-card"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <h2 className="text-3xl text-blue-800">Software Development</h2>
-            <p>I specialize in creating efficient, scalable, and robust software solutions using modern web technologies like React, Node.js, and Next.js. Whether you need a full-stack application or a simple frontend, I can help.</p>
-          </motion.div>
-          <motion.div 
-            className="service-card"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <h2 className="text-3xl text-blue-800">Technical Writing</h2>
-            <p>I can provide clear, concise technical documentation, tutorials, and blog posts to help your users or team understand your software product. I have experience writing for both technical and non-technical audiences.</p>
-          </motion.div>
-        </div>
+<div className="flex flex-col space-y-4 mt-8">
+  <motion.div 
+    className="service-card bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white p-6 rounded-lg shadow-lg"
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+  >
+    <h2 className="text-3xl">Software Development</h2>
+    <p>I specialize in creating efficient, scalable, and robust software solutions using modern web technologies like React, Node.js, and Next.js. Whether you need a full-stack application or a simple frontend, I can help.</p>
+  </motion.div>
+  <motion.div 
+    className="service-card bg-gradient-to-r from-green-400 via-blue-500 to-purple-500 text-white p-6 rounded-lg shadow-lg"
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+  >
+    <h2 className="text-3xl">Technical Writing</h2>
+    <p>I can provide clear, concise technical documentation, tutorials, and blog posts to help your users or team understand your software product. I have experience writing for both technical and non-technical audiences.</p>
+  </motion.div>
+</div>
+
+
       </Container>
     </motion.div>
   )
