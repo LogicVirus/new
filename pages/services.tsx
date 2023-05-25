ef49550bbc844fdb1fd5ab90cb02849e65d7517a
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-25 13:10:59 -0400

    modified:   pages/services.tsx

--- pages/services.tsx
+++ pages/services.tsx
@@ -18,35 +18,24 @@ function ServicesPage() {
         >
           Services
         </motion.h1>
-       // ...rest of your code...
-
-<div className="flex flex-col space-y-10 mt-8">
-  <motion.div 
-    className="service-card bg-gray-100 p-6 rounded-lg shadow-lg flex items-start space-x-4"
-    whileHover={{ scale: 1.05 }}
-    whileTap={{ scale: 0.95 }}
-  >
-    <img src="/software-development-icon.png" alt="Software Development Icon" className="w-12 h-12" />
-    <div>
-      <h2 className="text-2xl font-semibold text-blue-800">Software Development</h2>
-      <p className="mt-2">I specialize in creating efficient, scalable, and robust software solutions using modern web technologies like React, Node.js, and Next.js. Whether you need a full-stack application or a simple frontend, I can help.</p>
-    </div>
-  </motion.div>
-  <motion.div 
-    className="service-card bg-gray-100 p-6 rounded-lg shadow-lg flex items-start space-x-4"
-    whileHover={{ scale: 1.05 }}
-    whileTap={{ scale: 0.95 }}
-  >
-    <img src="/technical-writing-icon.png" alt="Technical Writing Icon" className="w-12 h-12" />
-    <div>
-      <h2 className="text-2xl font-semibold text-blue-800">Technical Writing</h2>
-      <p className="mt-2">I can provide clear, concise technical documentation, tutorials, and blog posts to help your users or team understand your software product. I have experience writing for both technical and non-technical audiences.</p>
-    </div>
-  </motion.div>
-</div>
-
-// ...rest of your code...
-
+        <div className="flex flex-col space-y-4 mt-8">
+          <motion.div 
+            className="service-card"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <h2 className="text-3xl text-blue-800">Software Development</h2>
+            <p>I specialize in creating efficient, scalable, and robust software solutions using modern web technologies like React, Node.js, and Next.js. Whether you need a full-stack application or a simple frontend, I can help.</p>
+          </motion.div>
+          <motion.div 
+            className="service-card"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <h2 className="text-3xl text-blue-800">Technical Writing</h2>
+            <p>I can provide clear, concise technical documentation, tutorials, and blog posts to help your users or team understand your software product. I have experience writing for both technical and non-technical audiences.</p>
+          </motion.div>
+        </div>
       </Container>
     </motion.div>
   )
