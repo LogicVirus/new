3b950e26f5776be47af7b9d9ed01f3975a978eec
LogicVirus <98907510+LogicVirus@users.noreply.github.com>
2023-05-26 23:14:26 -0400

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { motion, useAnimation } from 'framer-motion';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Container from '../components/container'
 import Image from 'next/image'
 import { Analytics } from '@vercel/analytics/react';
@@ -7,6 +7,10 @@ import { Analytics } from '@vercel/analytics/react';
 function HomePage() {
   const twitterLink = "https://twitter.com/joshperry0";
   const flickrLink = "https://www.flickr.com/photos/191854139@N02/";
+
+  const [laserFired, setLaserFired] = useState(false);
+  const [warpEngaged, setWarpEngaged] = useState(false);
+  const [forceFieldDeployed, setForceFieldDeployed] = useState(false);
 
   const controlsFlickr = useAnimation();
   const controlsTwitter = useAnimation();
@@ -28,6 +32,21 @@ function HomePage() {
     sequence(controlsTwitter);
   }, [controlsFlickr, controlsTwitter]);
 
+  const fireLaser = () => {
+    setLaserFired(true);
+    setTimeout(() => setLaserFired(false), 500);
+  };
+
+  const engageWarpDrive = () => {
+    setWarpEngaged(true);
+    setTimeout(() => setWarpEngaged(false), 1000);
+  };
+
+  const deployForceField = () => {
+    setForceFieldDeployed(true);
+    setTimeout(() => setForceFieldDeployed(false), 2000);
+  };
+
   return (
     <>
       <motion.div 
@@ -43,7 +62,7 @@ function HomePage() {
             animate={{ y: 0 }}
             transition={{ type: 'spring', stiffness: 120 }}
           >
-            Josh Perry
+            Captain Josh Perry
           </motion.h1>
           <motion.p 
             className="text-2xl text-center text-yellow-600 mt-6"
@@ -51,36 +70,70 @@ function HomePage() {
             animate={{ opacity: 1 }}
             transition={{ delay: 1, duration: 1 }}
           >
-            Greetings, explorers! Welcome to my space.
+            Greetings, space explorers! Welcome to my spaceship.
           </motion.p>
+
+          <motion.div 
+            className="text-2xl text-center text-green-500 mt-6"
+            initial={{ opacity: 0 }}
+            animate={laserFired ? { opacity: 1 } : { opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            Lasers fired!
+          </motion.div>
+
+          <motion.div 
+            className="text-2xl text-center text-blue-500 mt-6"
+            initial={{ opacity: 0 }}
+            animate={warpEngaged ? { opacity: 1 } : { opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Engaging warp drive!
+          </motion.div>
+
+          <motion.div 
+            className="text-2xl text-center text-purple-500 mt-6"
+            initial={{ opacity: 0 }}
+            animate={forceFieldDeployed ? { opacity: 1 } : { opacity: 0 }}
+            transition={{ duration: 2 }}
+          >
+            Deploying force field!
+          </motion.div>
+
           <div className="flex flex-col space-y-4 mt-8">
-            <motion.a 
-              href={twitterLink} 
-              target="_blank"
-              className="py-1 px-2 rounded text-lg flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-600 text-white w-auto px-5"
+            <motion.button 
+              className="py-1 px-2 rounded text-lg flex items-center justify-center bg-gradient-to-r from-red-500 to-orange-500 text-white w-auto px-5"
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
-              animate={controlsTwitter}
+              onClick={fireLaser}
             >
-              Follow me on Twitter
-            </motion.a>
-            <motion.a 
-              href={flickrLink} 
-              target="_blank"
+              Fire Lasers
+            </motion.button>
+
+            <motion.button 
+              className="py-1 px-2 rounded text-lg flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500 text-white w-auto px-5"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              onClick={engageWarpDrive}
+            >
+              Engage Warp Drive
+            </motion.button>
+
+            <motion.button 
               className="py-1 px-2 rounded text-lg flex items-center justify-center bg-gradient-to-r from-purple-500 to-blue-500 text-white w-auto px-5"
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
-              animate={controlsFlickr}
+              onClick={deployForceField}
             >
-              Explore my Flickr Gallery
-            </motion.a>
+              Deploy Force Field
+            </motion.button>
           </div>
         </Container>
 
         <div className="container max-w-4xl m-auto mt-15">
           <Image
-            src="/qwer.png"
-            alt="A nebula signifying the uncharted territories of software development"
+            src="/spaceship.png"
+            alt="A spaceship exploring the vastness of the universe"
             width={1920 / 2}
             height={1280 / 2}
           />
